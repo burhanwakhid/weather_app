5fa39c3eb0a32b0a4dd74afa23a2bb260dfb1287
burhanwakhid <burhanudinwakhid@student.uns.ac.id>
2020-01-10 15:20:08 +0700

    add some comment

--- lib/core/constant/constant.dart
+++ lib/core/constant/constant.dart
@@ -1,2 +1,3 @@
 const String apiOpenWeather = 'http://api.openweathermap.org/data/2.5/weather?';
 const String keyOpenWeather = '77bb2790c54d4f2cc40e90e1dc5ab6a9';
+// http://api.openweathermap.org/data/2.5/weather?q=sukoharjo&appid=77bb2790c54d4f2cc40e90e1dc5ab6a9#
