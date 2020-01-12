4e7c233e22b20496a33c8791ef9f153ddb8b0b70
Roid Muhyid Rabbani <roid.rabbani@gmail.com>
2020-01-12 11:56:42 +0700

    fix pull to request

--- lib/layouts/views/home_page.dart
+++ lib/layouts/views/home_page.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 import 'package:weather_app/core/view_models/weather_vm.dart';
+import 'package:pull_to_refresh/pull_to_refresh.dart';
 
 class HomePage extends StatelessWidget {
   @override
@@ -21,26 +22,43 @@ class _HomePageViewState extends State<HomePageView> {
 
   TextEditingController cityController = TextEditingController();
 
-  final GlobalKey<RefreshIndicatorState> _refreshIndicatorKey = new GlobalKey<RefreshIndicatorState>();
+  // final GlobalKey<RefreshIndicatorState> _refreshIndicatorKey = new GlobalKey<RefreshIndicatorState>();
+  RefreshController _refreshController =
+      RefreshController(initialRefresh: false);
   
   @override
   void initState() {
-    // Provider.of<WeatherProvider>(context, listen: false).getDataWeather();
+    Provider.of<WeatherProvider>(context, listen: false).getDataWeather();
     super.initState();
-    WidgetsBinding.instance
-      .addPostFrameCallback((_) => _refreshIndicatorKey.currentState.show());
+    // // WidgetsBinding.instance
+    // //   .addPostFrameCallback((_) => _refreshIndicatorKey.currentState.show());
+    // _refresh();
   }
 
-  Future<Null> _refresh() {
-   return Provider.of<WeatherProvider>(context, listen: false).getDataWeather();
-  }
+  // Future<Null> _refresh() async{
+  //    _refreshIndicatorKey.currentState?.show(atTop: false);
+  //   await Future.delayed(Duration(seconds: 2));
+  //  return Provider.of<WeatherProvider>(context, listen: false).getDataWeather();
+  // }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: RefreshIndicator(
-        key: _refreshIndicatorKey,
-        onRefresh: _refresh,
+      body: SmartRefresher(
+        controller: _refreshController,
+        enablePullUp: true,
+        header: WaterDropHeader(),
+        onRefresh: () async {
+          await Future.delayed(Duration(milliseconds: 1000));
+          Provider.of<WeatherProvider>(context, listen: false).getDataWeather();
+          if (mounted) setState(() {});
+          _refreshController.refreshCompleted();
+        },
+        onLoading: () async {
+          await Future.delayed(Duration(milliseconds: 1000));
+          if (mounted) setState(() {});
+          _refreshController.loadFailed();
+        },
         child: SafeArea(
           child: Container(
             child: Consumer<WeatherProvider>(
